1c877b3694e170d9a134b8c088cf6f9f32a73d74
Olivia <livie7c@gmail.com>
2022-02-22 14:16:39 +0100

    fix(css): use className in App

--- client/ui/src/App.tsx
+++ client/ui/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <section class="section">
-      <div class="container">
-        <h1 class="title">Hello World</h1>
-        <p class="subtitle">
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Hello World</h1>
+        <p className="subtitle">
           My first website with <strong>Bulma</strong>!
         </p>
       </div>
